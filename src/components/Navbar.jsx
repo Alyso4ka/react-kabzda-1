cd55886f6817e7a134968b00d211b764b0802862
Alyaso4ka <minety1997@mail.ru>
2020-03-06 13:43:35 +0300

    samurai 14

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
-const Navbar= () => {
-    return <nav className='nav'>
-        <div>
+// let s = {
+//    'nav': 'Navbar_nav__2x8Oz',
+//    'item': 'Navbar_item__3PprI';
+// }
+
+const Navbar = () => {
+    return <nav className={s.nav}>
+        <div className={s.item}>
             <a>Profile</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Massages</a>
-        </div><div>
+        </div>
+        <div className={s.item}>
             <a>News</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a>Music</a>
         </div>
-        <div>
+        <div className={s.item}>
             <a> Settings </a>
         </div>
     </nav>
 }
-   export default Navbar;
+export default Navbar;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
     <img src='https://www.w3schools.com/howto/img_snow_wide.jpg' />
     <div>
       <div>
@@ -12,11 +13,11 @@ const Profile = () => {
         <div>
           New post
         </div>
-        <div>
-          <div>
+        <div className={s.posts}>
+          <div className={s.item}>
             post 1
           </div>
-          <div>
+          <div className={s.item}>
             post 2
           </div>
         </div>
