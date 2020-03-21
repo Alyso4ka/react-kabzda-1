d26b06c858c7ba020b76779ca321101acbe6cac7
Alyaso4ka <minety1997@mail.ru>
2020-03-21 10:36:12 +0300

    samurai 29

--- src/index.js
+++ src/index.js
@@ -3,35 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-let posts= [
-    {message: "Hi, how are you?", likesCount: 22, id: 1},
-    {message: "It's my first post", likesCount: 55, id: 2}
+import state from './redux/state';
 
 
-];
-
-
-let dialogs = [
-    {name: "Dimych", id: 1},
-    {name: "Andrey", id: 2},
-    {name: "Sveta", id: 3},
-    {name: "Sacha", id: 4},
-    {name: "Victor", id: 5},
-    {name: "Valera", id: 6},
-];
-
-let messages = [
-    {message: "Hi!", id: 1},
-    {message: "How is your it-kamasutra?", id: 2},
-    {message: "Yo", id: 3},
-    {message: "Yo", id: 4},
-    {message: "Yo", id: 5},
-];
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages}/>, document.getElementById('root'));
+ReactDOM.render(<App state={state}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
---
+++ src/redux/state.js
@@ -0,0 +1,37 @@
+let state = {
+
+    profilePage: {
+        posts: [
+            {message: "Hi, how are you?", likesCount: 22, id: 1},
+            {message: "It's my first post", likesCount: 55, id: 2}
+
+
+        ],
+
+
+    },
+
+
+    dialogsPage: {
+
+        dialogs: [
+            {name: "Dimych", id: 1},
+            {name: "Andrey", id: 2},
+            {name: "Sveta", id: 3},
+            {name: "Sacha", id: 4},
+            {name: "Victor", id: 5},
+            {name: "Valera", id: 6},
+        ],
+        messages: [
+            {message: "Hi!", id: 1},
+            {message: "How is your it-kamasutra?", id: 2},
+            {message: "Yo", id: 3},
+            {message: "Yo", id: 4},
+            {message: "Yo", id: 5},
+        ]
+    }
+
+
+}
+
+export default state;
