299731bdea6ba7468090d097aa12c6e04b011b34
Alyaso4ka <minety1997@mail.ru>
2020-04-10 12:05:06 +0300

    samurai 39

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
 
@@ -57,7 +60,7 @@ let store = {
     },
 
     dispatch(action) { // { type: 'ADD-POST' }
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 message: this._state.profilePage.newPostText,
                 likesCount: 0,
@@ -67,7 +70,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -76,7 +79,18 @@ let store = {
 
 };
 
+export const addPostActionCreator = () => {
+    return {
+        type: ADD_POST
+    }
+}
 
+export const updateNewPostTextActionCreator = (text) => {
+    return {
+        type: UPDATE_NEW_POST_TEXT,
+        newText: text
+    }
+}
 export default store;
 window.store = store;
 
