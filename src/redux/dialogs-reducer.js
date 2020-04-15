5fc0ee91062cc7ab1b7cba2d40018592effad74a
Женя <minety1997@mail.ru>
2020-04-15 11:42:09 +0300

    samurai 42 install redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,8 +1,28 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
+let initialState = {
 
-const dialogsReducer = (state, action) => {
+    dialogs: [
+        {name: "Dimych", id: 1},
+        {name: "Andrey", id: 2},
+        {name: "Sveta", id: 3},
+        {name: "Sacha", id: 4},
+        {name: "Victor", id: 5},
+        {name: "Valera", id: 6},
+    ],
+    messages: [
+        {message: "Hi!", id: 1},
+        {message: "How is your it-kamasutra?", id: 2},
+        {message: "Yo", id: 3},
+        {message: "Yo", id: 4},
+        {message: "Yo", id: 5},
+    ],
+    newMessageBody: ''
+}
+
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,16 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        {message: "Hi, how are you?", likesCount: 22, id: 1},
+        {message: "It's my first post", likesCount: 55, id: 2}
+    ],
+
+    newPostText: 'it-kamasutra.com'
+}
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
