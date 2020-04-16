f06fd1bc7a7202f9bfc8319de4e3cf7898836f50
Женя <minety1997@mail.ru>
2020-04-16 11:12:09 +0300

    samurai 43 container componnet

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {newMessageText} from "./redux/store";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 
 const App = (props) => {
@@ -20,11 +21,11 @@ const App = (props) => {
             <Header/>
             <Navbar/>
             <div class='app-wrapper-content'>
-                <Route path='/dialogs' render={() => <Dialogs store={props.store}
+                <Route path='/dialogs' render={() => <DialogsContainer store={props.store}
+
                                                               />}/>
                 <Route path='/profile' render={() => <Profile
-                    profilePage={props.state.profilePage}
-                    dispatch={props.dispatch}
+                    store={props.store}
                 />}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
