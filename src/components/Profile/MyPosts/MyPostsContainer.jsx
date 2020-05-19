8673d10a25514fa7832f8f71b3696885cb9bced5
Женя <minety1997@mail.ru>
2020-05-19 11:34:04 +0300

    refactoring mypostsconainer

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -6,29 +6,36 @@ import StoreContext from "../../../StoreContext";
 
 const MyPostsContainer = (props) => {
 
-    // let state = props.store.getState();
 
-    let addPost = () => {
-        props.store.dispatch(addPostActionCreator());
-    };
 
-    let onPostChange = (text) => {
-
-        let action = updateNewPostTextActionCreator(text);
-        props.store.dispatch(action);
-    };
 
     return (
         <StoreContext.Consumer> {
-            (store) => (
-                <MyPosts updateNewPostText={onPostChange}
+            (store) => {
+                let state = props.store.getState();
+
+
+                let addPost = () => {
+                    props.store.dispatch(addPostActionCreator());
+                };
+
+                let onPostChange = (text) => {
+
+                    let action = updateNewPostTextActionCreator(text);
+                    props.store.dispatch(action);
+                };
+
+               return <MyPosts updateNewPostText={onPostChange}
                          addPost={addPost}
-                         posts={store.getState().profilePage.posts}
-                         newPostText={store.getState().profilePage.newPostText}/>)
+                         posts={state.profilePage.posts}
+                         newPostText={state.profilePage.newPostText}/>}
         }
         </StoreContext.Consumer>
+
                 )
                 }
+
+
 
 
 
