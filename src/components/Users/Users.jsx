0b1c2ae28b842b78f6e9d60fe64bb318cc49330b
Женя <minety1997@mail.ru>
2020-05-26 14:25:22 +0300

    49 PS ERR

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,82 @@
 import React from 'react';
+import styles from './users.module.css';
 
-let Users = () => {
-    return (
-        <div>USERS WILL BE HERE!</div>
+
+let Users = (props) => {
+
+if (props.users.length === 0) {
+    props.setUsers([
+            {
+                id: 1,
+                photoUrl: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s600',
+                followed: true,
+                fullName: 'Zhenya',
+                status: 'Hard work',
+                location: {city: 'Minsk', country: 'Belarus'}
+            },
+            {
+                id: 2,
+                photoUrl: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s600',
+                followed: true,
+                fullName: 'Katya',
+                status: 'Hard work every day',
+                location: {city: 'Moscow', country: 'Russia'}
+            },
+            {
+                id: 3,
+                photoUrl: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s600',
+                followed: false,
+                fullName: 'Andrey',
+                status: 'Chill',
+                location: {city: 'Kiev', country: 'Ukraine'}
+            },
+            {
+                id: 4,
+                photoUrl: 'https://avatars.mds.yandex.net/get-pdb/1352825/a5f8fbd7-515e-49f8-81d6-be55a2daac92/s600',
+                followed: false,
+                fullName: 'Marina',
+                status: 'Weekend',
+                location: {city: 'New York', country: 'America'}
+            }
+        ]
     )
+}
+
+
+    return (
+    <div>
+            {
+                props.users.map ( u => <div key={u.id}>
+    <span>
+        <div>
+            <img className={styles.userPhoto} src={u.photoUrl}/>
+        </div>
+        <div>
+            {u.followed
+                ? <button onClick={() => {
+                    props.unfollow(u.id)
+                }}>Unfollow</button>
+                : <button onClick={() => {
+                    props.follow(u.id)
+                }}>Follow</button>}
+
+        </div>
+    </span>
+                    <span>
+        <span>
+            <div>{u.fullName}</div>
+            <div>{u.status}</div>
+        </span>
+        <span>
+            <div>{u.location.country}</div>
+            <div>{u.location.city}</div>
+        </span>
+    </span>
+                </div>)
+            }
+        </div>
+    )
+
 }
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,13 +1,10 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
 
 let initialState = {
 
     users: [
-        {id: 1, followed: true, fullName: 'Zhenya', status: 'Hard work', location: {city: 'Minsk', country: 'Belarus'}},
-        {id: 2, followed: true, fullName: 'Katya', status: 'Hard work every day', location: {city: 'Moscow', country: 'Russia'}},
-        {id: 3, followed: false, fullName: 'Andrey', status: 'Chill', location: {city: 'Kiev', country: 'Ukraine'}},
-        {id: 4, followed: false, fullName: 'Marina', status: 'Weekend', location: {city: 'New York', country: 'America'}},
 
     ],
 
@@ -18,7 +15,7 @@ const usersReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case FOLLOW:
-            let stateCopy= {
+           return {
                 ...state,
                 // users:[...state.users], одно и тоже что и мап, но нам надо вернуть не всех юзеров не тронутыми, одного нужно изменить
                 users: state.users.map (u => {
@@ -29,9 +26,22 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
 
-
         case UNFOLLOW:
+            return {
+                ...state,
+                // users:[...state.users], одно и тоже что и мап, но нам надо вернуть не всех юзеров не тронутыми, одного нужно изменить
+                users: state.users.map (u => {
+                    if (u.id === action.userId) {
+                        return {...u, followed: false}
+                    }
+                    return u;
+                })
+            }
 
+
+        case SET_USERS:{
+            return {...state, users: [...state.users, ...action.users]}
+        }
         default:
             return state;
     }
@@ -39,7 +49,8 @@ const usersReducer = (state = initialState, action) => {
 }
 
 export const followAC = (userId) => ({type: FOLLOW, userId})
-export const unFollowAC = (userId) => ({type: UNFOLLOW, userId})
+export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
+export const setUsersAC = (user) => ({type: SET_USERS, user})
 
 
 export default usersReducer;
