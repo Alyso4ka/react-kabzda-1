b171889a4692eef68faf933ba67257fa162af103
Женя <minety1997@mail.ru>
2020-06-02 12:32:15 +0300

    class Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,21 +4,23 @@ import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
 
-let Users = (props) => {
-    let getUsers = () => {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
 
-        if (props.users.length === 0) {
             axios.get('https://social-network.samuraijs.com/api/1.0/users')
                 .then(response => {
-                    props.setUsers(response.data.items)
+                    this.props.setUsers(response.data.items)
                 })
         }
-    }
 
-    return <div>
-        <button onClick={getUsers}>GET USERS</button>
-        {
-            props.users.map(u => <div key={u.id}>
+    
+
+    render() {
+        return <div>
+
+            {
+                this.props.users.map(u => <div key={u.id}>
     <span>
         <div>
             <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto}/>
@@ -26,15 +28,15 @@ let Users = (props) => {
         <div>
             {u.followed
                 ? <button onClick={() => {
-                    props.unfollow(u.id)
+                    this.props.unfollow(u.id)
                 }}>Unfollow</button>
                 : <button onClick={() => {
-                    props.follow(u.id)
+                    this.props.follow(u.id)
                 }}>Follow</button>}
 
         </div>
     </span>
-                <span>
+                    <span>
         <span>
             <div>{u.name}</div>
             <div>{u.status}</div>
@@ -44,11 +46,12 @@ let Users = (props) => {
             <div>{'u.location.city'}</div>
         </span>
     </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
 
-
+    }
 }
+
 
 export default Users;
