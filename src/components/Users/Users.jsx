62b9a6e159a18139e32130c5efe0a2489bafa68d
Женя <minety1997@mail.ru>
2020-06-04 15:13:14 +0300

    55 finished

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,9 +7,16 @@ import userPhoto from '../../assets/images/user.png';
 class Users extends React.Component {
 
         componentDidMount() {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+
+        }
+
+
+        onPageChanged = (pageNumber) => {
+           this.props.setCurrentPage(pageNumber);
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
                 .then(response => {
                     this.props.setUsers(response.data.items)
+                    this.props.setTotalUsersCount(response.data.totalCount)
                 })
         }
 
@@ -27,7 +34,7 @@ class Users extends React.Component {
         return <div>
             <div>
                 {pages.map (p => {
-                    return <span className={this.props.currentPage === p && styles.selectedPage}>{p}</span>
+                    return <span onClick={(e) => {this.onPageChanged(p)}} className={this.props.currentPage === p && styles.selectedPage}>{p}</span>
                 })}
             </div>
 
