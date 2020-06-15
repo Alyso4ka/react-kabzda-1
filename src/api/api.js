8d69c4484a1acdd3aa18df034a1a3c9043c59c68
Женя <minety1997@mail.ru>
2020-06-15 14:29:15 +0300

    63 ff

---
+++ src/api/api.js
@@ -0,0 +1,35 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'b06608a6-b289-49e3-a247-1876a702d6e4'
+    }
+
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
+            .then(response => {
+                return response.data
+            });
+    }
+}
+
+
+export const getUsers = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
+        .then(response => {
+            return response.data
+        });
+}
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+    return instance.get(`follow?page=${currentPage}&count=${pageSize}`,)
+        .then(response => {
+            return response.data
+        });
+}
+
