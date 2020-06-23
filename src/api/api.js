08e109e0fb4d676d469e4776bbb79c804c1d692c
Женя <minety1997@mail.ru>
2020-06-23 12:32:28 +0300

    refactoring api

--- src/api/api.js
+++ src/api/api.js
@@ -26,17 +26,4 @@ export const usersAPI = {
 }
 
 
-export const getUsers = (currentPage = 1, pageSize = 10) => {
-    return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
-        .then(response => {
-            return response.data
-        });
-}
-
-export const getUsers2 = (currentPage = 1, pageSize = 10) => {
-    return instance.get(`follow?page=${currentPage}&count=${pageSize}`,)
-        .then(response => {
-            return response.data
-        });
-}
 
