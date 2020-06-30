14fca27436d7efab95dca6e439a5a5786551cc56
Женя <minety1997@mail.ru>
2020-06-30 16:55:55 +0300

    login form ui

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+const LoginForm = (props) => {
+    return (
+    <form>
+<div>
+    <input placeholder={'login'}/>
+</div>
+<div>
+    <input placeholder={'password'}/>
+</div>
+<div>
+    <input type={'checkbox'}/> remember me
+</div>
+<div>
+    <button>login</button>
+</div>
+    </form>
+    )
+}
+
+
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>LOGIN</h1>
+        <LoginForm/>
+    </div>
 }
 
 export default Login;
