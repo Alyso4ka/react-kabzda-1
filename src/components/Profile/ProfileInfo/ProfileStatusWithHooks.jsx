1eeb16f5cedc931e9f107ab0d220538b9accc43e
Женя <minety1997@mail.ru>
2020-07-18 13:34:20 +0300

    React JS - hook, useState, хуки

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import s from './ProfileInfo.module.css';
+
+
+const ProfileStatusWithHooks = (props) => {
+
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status)
+
+
+const activateMode = () => {
+    setEditMode(true)
+}
+
+    const deActivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status);
+    }
+
+   const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            { !editMode &&
+            <div>
+                <span onDoubleClick={activateMode}>{props.status || 'NO STATUS'}</span>
+            </div>
+            }
+            {editMode &&
+            <div>
+                <input
+                    onBlur={deActivateEditMode}
+                    autoFocus={true}
+                    onChange={onStatusChange}
+                    value={status}
+                    />
+            </div>
+            }
+        </div>
+    )
+}
+
+
+
+export default ProfileStatusWithHooks;
+
+
